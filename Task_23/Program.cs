0ce5a4a2a399305b3bcd2f35b82292454e3694ed
Jeka_A <antimur4ik@yandex.ru>
2023-06-23 17:57:26 +0300

    Таблица кубов множества N

---
+++ Task_23/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+////////////
+//  Решил через while
+
+Console.Write($"Данно число: ");
+int N = new Random().Next(1, 10);
+int i = 1;
+Console.WriteLine(N);
+Console.Write($"Кубы множества от 1 до {N}: ");
+while (i <= N)
+{
+    Console.Write($"{Math.Pow(i,3)} ");
+    i ++;
+}
+//////////////////////////
+//Вариант, как на занятии
+int RandomNumber(int min, int max) 
+{
+    int result = new Random().Next(min,max+1);
+    return result;
+   
+} 
+void Print (string text)
+{ 
+    Console.WriteLine(text); 
+} 
+void GetSquare(int rand){ 
+    for (int i=1;i<=rand;i++)
+    { 
+        Console.Write($"{Math.Pow(i,3)}, "); 
+    } 
+} 
+int rand = RandomNumber(1, 10); 
+Print($"{rand}"); 
+GetSquare(rand);
